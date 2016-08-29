66ad97361523441fbced312cb871f1541d8e45df
swordsmanLonely <1054917499@qq.com>
2016-08-29 18:53:03 +0800

    第一次提交

---
+++ app/src/main/java/com/example/administrator/tenbuy/main/DeserveFragment.java
@@ -0,0 +1,34 @@
+package com.example.administrator.tenbuy.main;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.tenbuy.R;
+import com.example.administrator.tenbuy.base.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class DeserveFragment extends BaseFragment{
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_deserve;
+    }
+
+}
---
+++ app/src/main/java/com/example/administrator/tenbuy/main/TenbuyFragment.java
@@ -0,0 +1,35 @@
+package com.example.administrator.tenbuy.main;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.tenbuy.R;
+import com.example.administrator.tenbuy.base.BaseFragment;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TenbuyFragment extends BaseFragment {
+
+
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_tenbuy;
+    }
+
+}
